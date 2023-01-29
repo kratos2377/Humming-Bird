19d63b6ac3deeed5f7b45d92ee15038f6affca46
kratos2377 <shobityadav23@gmail.com>
2023-01-29 23:00:59 +0530

    Simulation wasm added

--- src-tauri/simulation-wasm/src/lib.rs
+++ src-tauri/simulation-wasm/src/lib.rs
@@ -1,14 +1,52 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub use self::{animal::*, food::*, world::*};
+use lib_simulation as sim;
+use rand::{prelude::ThreadRng, thread_rng};
+use serde::Serialize;
+use wasm_bindgen::prelude::*;
+
+mod animal;
+mod food;
+mod world;
+
+#[wasm_bindgen]
+pub struct Simulation {
+    rng: ThreadRng,
+    sim: sim::Simulation,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[wasm_bindgen]
+impl Simulation {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        let mut rng = thread_rng();
+        let sim = sim::Simulation::random(&mut rng);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        Self { rng, sim }
+    }
+
+    pub fn world(&self) -> JsValue {
+        let world = World::from(self.sim.world());
+        JsValue::from_serde(&world).unwrap()
+    }
+
+    pub fn step(&mut self) {
+        self.sim.step(&mut self.rng);
+    }
+
+    pub fn train(&mut self) -> String {
+        let stats = self.sim.train(&mut self.rng);
+
+        format!(
+            "min={:.2}, max={:.2}, avg={:.2}",
+            stats.min_fitness(),
+            stats.max_fitness(),
+            stats.avg_fitness(),
+        )
+    }
+}
+
+impl Default for Simulation {
+    fn default() -> Self {
+        Self::new()
     }
 }
