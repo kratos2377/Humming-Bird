6f62b88032227bb82862a0d7ca7dbf2c47232e79
kratos2377 <shobityadav23@gmail.com>
2023-01-31 00:39:35 +0530

    Minor changes in web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,33 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import * as sim from './js/index'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div id="container">
+    <div id="terminal-panel" className="panel">
+      <textarea id="terminal-stdout" ></textarea>
+      <input id="terminal-stdin"/>
     </div>
+    <div id="viewport-panel" className="panel">
+      <canvas id="viewport"></canvas>
+    </div>
+  </div>
   )
 }
 
