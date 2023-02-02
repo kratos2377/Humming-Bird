76bd03c62c77d37de166b3b54f84ba9168bf2a5a
kratos2377 <shobityadav23@gmail.com>
2023-02-02 21:23:42 +0530

    Home.jsx fixed and css added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,8 +3,7 @@ import './App.css'
 import Home from './components/Home'
 import Simulation from './components/Simulation'
 import { Button } from '@mui/material';
-// import init from './lib_simulation_wasm_bg.wasm?init'
-//  import './index.js'
+
 
 function App() {
 
@@ -16,7 +15,7 @@ const [loadSim , setLoadSim] = useState(false)
   return (
     <>
     {!loadSim ? <Home/> : <Simulation/>}
-    {!loadSim ? <Button onClick={setSim} variant="outlined">Start Simulation</Button> : <></>}
+    {!loadSim ? <Button  onClick={setSim} variant="outlined">Start Simulation</Button> : <></>}
     </>
   )
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,10 +1,23 @@
+import { Typography , Box } from '@mui/material';
 import React , {useState} from 'react';
-
+import './css/home.css'
+import nn from '../assets/nn.svg'
 const Home = () => {
     return (
-        <>
-          
-        </>
+        <div className="content">
+    <Box sx={{width: "100%" , maxWidth: 1000}}>
+    <div >
+      <img src={nn} sizes={900} alt="Your SVG" />
+    </div>
+    </Box>
+
+          <Box sx={{width: "100%" , maxWidth: 500}}>
+          <Typography color="common.white">
+       The follo
+      </Typography>
+
+          </Box>
+        </div>
     );
 }
 
